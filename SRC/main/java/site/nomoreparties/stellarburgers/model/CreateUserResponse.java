23fe7b8bdb204cb46745e7fdc944c4b354f7f918
mailffgfghhtt-prog <mailffgfghhtt@gmail.com>
2026-02-24 15:12:36 +0300

    Delete SRC directory

--- SRC/main/java/site/nomoreparties/stellarburgers/model/CreateUserResponse.java
+++
@@ -1,9 +0,0 @@
-package site.nomoreparties.stellarburgers.model;
-import lombok.*;
-@Getter
-public class CreateUserResponse {
-    private String accessToken;
-    private String refreshToken;
-    private String success;
-    private User user;
-}
--- SRC/main/java/site/nomoreparties/stellarburgers/model/GetIngredientsResponse.java
+++
@@ -1,8 +0,0 @@
-package site.nomoreparties.stellarburgers.model;
-import java.util.List;
-import lombok.*;
-@Getter
-public class GetIngredientsResponse {
-    private boolean success;
-    private List<Ingredient> data;
-}
